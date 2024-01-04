eeeabf506cbd3eb3a1effd204059f09a42367de2
Basavaraj Chinchakhandi <basumohan96@gmail.com>
2024-01-04 09:02:49 +0530

    reCaptcha Validations Added

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 
+let captchaChecked = false;
 
+function beforesubmit(event) {
+    if (captchaChecked) {
+        let outputDate = document.querySelector(".outputdate");
+        let leadDate = document.getElementById("leadDate");
+        console.log("leadDate.value", leadDate.value); //string ---> date ("en_IN")
 
-function beforesubmit() {
-    let outputDate = document.querySelector(".outputdate");
-    let leadDate = document.getElementById("leadDate");
-    console.log("leadDate.value", leadDate.value); //string ---> date ("en_IN")
+        //(system.debug(User-Info.getLocale())
 
-    //(system.debug(User-Info.getLocale())
-
-    let formattedDate = new Date(leadDate.value).toLocaleDateString("en-IN");
-    outputDate.value = formattedDate;
-
+        let formattedDate = new Date(leadDate.value).toLocaleDateString("en-IN");
+        outputDate.value = formattedDate;
+    } else {
+        alert("Please check the reCaptcha Boc to submit the lead")
+        event.preventDefault();
+    }
 }
 
 function timestamp() {
@@ -20,4 +24,8 @@ function timestamp() {
         elems["ts"] = JSON.stringify(new Date().getTime());
         document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems);
     }
-} setInterval(timestamp, 500); 
+} setInterval(timestamp, 500);
+
+function captchasuccess() {
+    captchaChecked = true;
+}
